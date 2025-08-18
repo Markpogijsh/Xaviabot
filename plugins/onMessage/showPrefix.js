1862f31f34916567569e2af67d882a628da3b28c
Markpogijsh <mark123jwj@gmail.com>
2025-08-19 07:05:19 +0800

    Update showPrefix.js

--- plugins/onMessage/showPrefix.js
+++ plugins/onMessage/showPrefix.js
@@ -6,8 +6,6 @@ const langData = {
     }
 };
 
-const reactions = ["airkiss","angrystare","bite","bleh","blush","brofist","celebrate","cheers","clap","confused","cool","cry","cuddle","dance","drool","evillaugh","facepalm","handhold","happy","headbang","hug","huh","kiss","laugh","lick","love","mad","nervous","no","nom","nosebleed","nuzzle","nyah","pat","peek","pinch","poke","pout","punch","roll","run","sad","scared","shout","shrug","shy","sigh","sip","slap","sleep","slowclap","smack","smile","smug","sneeze","sorry","stare","stop","surprised","sweat","thumbsup","tickle","tired","wave","wink","woah","yawn","yay","yes"];
-
 async function onCall({ message, getLang, data }) {
     const messageBody = message.body.toLowerCase().trim();
     const prefixTriggers = [
@@ -22,7 +20,7 @@ async function onCall({ message, getLang, data }) {
         const randomReaction = reactions[Math.floor(Math.random() * reactions.length)];
 
         try {
-            const { data: gifData } = await axios.get(`https://api.otakugifs.xyz/gif?reaction=${randomReaction}`);
+            const { data: gifData } = await axios.get(`https://i.imgur.com/68S6RBS.jpeg`);
             const response = await axios.get(gifData.url, { responseType: 'stream' });
             await message.reply({
                 body: getLang("prefix", { prefix }),
